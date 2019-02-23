83185a0f8705be668f9580f895214210c569b18f
Alex Dodge <alexdodge@gmail.com>
2019-02-23 21:43:18 +0000

    As far as I can tell, we never don't add these two numbers.

--- models/Model.py
+++ models/Model.py
@@ -7,14 +7,16 @@ class Model(object):
             n_entities:int,
             n_relations:int,
             batch_size:int=5000,
-            negative_ent:int=1,
-            negative_rel:int=0):
+            n_negative:int=1,
 
+        # Number of entities in the training data
         self.n_entities = n_entities
+        # Number of relations in the training data
         self.n_relations = n_relations
+        # Training batch size
         self.batch_size = batch_size
-        self.negative_ent = negative_ent
-        self.negative_rel = negative_rel
+        # Number of negative examples per positive example
+        self.n_negative = n_negative
 
         # Construct TF graphs
 		with tf.name_scope("input"):
@@ -31,7 +33,7 @@ class Model(object):
 
     @property
     def batch_seq_size(self):
-        return self.batch_size*(1+self.negative_ent+self.negative_rel)
+        return self.batch_size*(1+self.n_negative)
 
 	def get_positive_instance(self, in_batch = True):
 		if in_batch:
@@ -51,15 +53,15 @@ class Model(object):
 
 	def get_all_instance(self, in_batch = False):
 		if in_batch:
-			return [tf.transpose(tf.reshape(self.batch_h, [1 + self.negative_ent + self.negative_rel, -1]), [1, 0]),\
-			tf.transpose(tf.reshape(self.batch_t, [1 + self.negative_ent + self.negative_rel, -1]), [1, 0]),\
-			tf.transpose(tf.reshape(self.batch_r, [1 + self.negative_ent + self.negative_rel, -1]), [1, 0])]
+			return [tf.transpose(tf.reshape(self.batch_h, [1 + self.n_negative, -1]), [1, 0]),\
+			tf.transpose(tf.reshape(self.batch_t, [1 + self.n_negative, -1]), [1, 0]),\
+			tf.transpose(tf.reshape(self.batch_r, [1 + self.n_negative, -1]), [1, 0])]
 		else:
 			return [self.batch_h, self.batch_t, self.batch_r]
 
 	def get_all_labels(self, in_batch = False):
 		if in_batch:
-			return tf.transpose(tf.reshape(self.batch_y, [1 + self.negative_ent + self.negative_rel, -1]), [1, 0])
+			return tf.transpose(tf.reshape(self.batch_y, [1 + self.n_negative, -1]), [1, 0])
 		else:
 			return self.batch_y
 
@@ -67,6 +69,13 @@ class Model(object):
 		return [self.predict_h, self.predict_t, self.predict_r]
 
 	def input_def(self):
+        '''
+        Input batches are passed in as three lists of integers and a list of
+        floats.  These four lists are aligned.
+
+        The first _batch_size_ entries are positive examples and the remaining
+        entries are negative examples.
+        '''
 		self.batch_h = tf.placeholder(tf.int64, [self.batch_seq_size])
 		self.batch_t = tf.placeholder(tf.int64, [self.batch_seq_size])
 		self.batch_r = tf.placeholder(tf.int64, [self.batch_seq_size])
@@ -74,9 +83,9 @@ class Model(object):
 		self.postive_h = tf.transpose(tf.reshape(self.batch_h[0:self.batch_size], [1, -1]), [1, 0])
 		self.postive_t = tf.transpose(tf.reshape(self.batch_t[0:self.batch_size], [1, -1]), [1, 0])
 		self.postive_r = tf.transpose(tf.reshape(self.batch_r[0:self.batch_size], [1, -1]), [1, 0])
-		self.negative_h = tf.transpose(tf.reshape(self.batch_h[self.batch_size:self.batch_seq_size], [self.negative_ent + self.negative_rel, -1]), perm=[1, 0])
-		self.negative_t = tf.transpose(tf.reshape(self.batch_t[self.batch_size:self.batch_seq_size], [self.negative_ent + self.negative_rel, -1]), perm=[1, 0])
-		self.negative_r = tf.transpose(tf.reshape(self.batch_r[self.batch_size:self.batch_seq_size], [self.negative_ent + self.negative_rel, -1]), perm=[1, 0])
+		self.negative_h = tf.transpose(tf.reshape(self.batch_h[self.batch_size:self.batch_seq_size], [self.n_negative, -1]), perm=[1, 0])
+		self.negative_t = tf.transpose(tf.reshape(self.batch_t[self.batch_size:self.batch_seq_size], [self.n_negative, -1]), perm=[1, 0])
+		self.negative_r = tf.transpose(tf.reshape(self.batch_r[self.batch_size:self.batch_seq_size], [self.n_negative, -1]), perm=[1, 0])
 		self.predict_h = tf.placeholder(tf.int64, [None])
 		self.predict_t = tf.placeholder(tf.int64, [None])
 		self.predict_r = tf.placeholder(tf.int64, [None])
